ac9ea7a4ba679a477f21911392d1ad2d2396cab4
yingxuanxuan <yingxuanxuan@hotmail.com>
2016-05-14 13:49:53 +0800

    get_server_by_tag

--- fabfile.py
+++ fabfile.py
@@ -57,3 +57,33 @@ def get_server_by_id(api_key, id):
     except BaseException as e:
         print(e)
         return None
+
+def get_server_by_tag(api_key, tag):
+    url = 'https://api.vultr.com/v1/server/list'
+
+    if tag:
+        url += '?tag=%s' % tag
+
+    req = request.Request(url)
+    req.add_header('API-Key', api_key)
+
+    try:
+        with request.urlopen(req) as f:
+            data = f.read()
+            if not data:
+                print('Read data fail.')
+                return None
+
+            obj = json.loads(data.decode('utf-8'))
+            if not obj:
+                print('Parser json fail.')
+                return None
+
+            if 0 == len(obj):
+                print('Get server error.')
+            else:
+                print('Get server success.')
+                return obj
+    except BaseException as e:
+        print(e)
+        return None
