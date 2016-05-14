bfadabab461d8902acbafa953674d16911db4aa1
yingxuanxuan <yingxuanxuan@hotmail.com>
2016-05-14 13:38:42 +0800

    Get server by id.

--- fabfile.py
+++ fabfile.py
@@ -27,3 +27,33 @@ def can_access(api_key):
         print('Access fail.')
         print(e)
         return False
+
+def get_server_by_id(api_key, id):
+    url = 'https://api.vultr.com/v1/server/list'
+
+    if id:
+        url += '?SUBID=%s' % id
+
+    req = request.Request(url)
+    req.add_header('API-Key', api_key)
+
+    try:
+        with request.urlopen(req) as f:
+            data = f.read()
+            if not data:
+                print('Read data fail.')
+                return None
+
+            obj = json.loads(data.decode('utf-8'))
+            if not obj:
+                print('Parser json fail.')
+                return None
+
+            if str(id) != obj.get('SUBID', None):
+                print('Get server error.')
+            else:
+                print('Get server success.')
+                return obj
+    except BaseException as e:
+        print(e)
+        return None
