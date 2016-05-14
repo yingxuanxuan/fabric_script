511d6e2a0133f73bc23431cb4280dbcac61a9dcf
yingxuanxuan <yingxuanxuan@hotmail.com>
2016-05-14 16:09:13 +0800

    Change project name.

--- fabfile.py
+++ fabfile.py
@@ -64,11 +64,8 @@ def get_server_by_id(api_key, id):
         logging.error(e)
         return None
 
-def get_server_by_tag(api_key, tag):
+def get_server_list(api_key):
     url = 'https://api.vultr.com/v1/server/list'
-
-    if tag:
-        url += '?tag=%s' % tag
 
     req = request.Request(url)
     req.add_header('API-Key', api_key)
@@ -88,11 +85,51 @@ def get_server_by_tag(api_key, tag):
                 logging.error('Parser json fail.')
                 return None
 
-            if 0 == len(obj):
-                logging.error('Get server error.')
-            else:
-                logging.info('Get server success.')
-                return obj
+            summary = {}
+            for server, info in obj:
+                for k, v in info:
+                    if k in ['SUBID',
+                             'os',
+                             'ram',
+                             'disk',
+                             'main_ip',
+                             'vcpu_count',
+                             'location',
+                             'default_password',
+                             'pending_charges',
+                             'status',
+                             'cost_per_month',
+                             'current_bandwidth_gb',
+                             'allowed_bandwidth_gb',
+                             'power_status',
+                             'server_state',
+                             'label']:
+                        summary[server][k] = v
+
+            logging.info('Get server list:')
+            logging.info(summary.dump(indent=2))
+            return obj
     except BaseException as e:
         logging.error(e)
         return None
+
+def get_regions_list():
+    pass
+
+def check_regions_availability(de_id):
+    pass
+
+def get_plans_list():
+    pass
+
+def get_os_list():
+    pass
+
+def create_server(api_key, label, dc_id, plan_id, os_id):
+    pass
+
+def destroy_server(label):
+    pass
+
+
+
