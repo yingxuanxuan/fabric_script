bd75ab48468588244f61ef0a75a6c2fa29f017ce
yingxuanxuan <yingxuanxuan@hotmail.com>
2016-05-22 18:30:34 +0800

    Complete shadowsocks.py

--- shadowsocks.py
+++ shadowsocks.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import logging; logging.basicConfig(level=logging.INFO)
+from fabric.api import reboot, sudo, settings
 
+def setup(port, password, method):
+    try:
+        sudo('hash yum')
+        sudo('hash python')
+        sudo('yum -y update')
+        sudo('yum -y install python-setuptools')
+        sudo('easy_install pip')
+        sudo('pip install shadowsocks')
+        sudo('hash ssserver')
 
-sudo /usr/bin/python2.7 /usr/local/bin/ssserver -c
-/etc/shadowssocks.json -d start
+        cmd = '/usr/bin/python /usr/bin/ssserver -p %s -k %s -m %s --user nobody -d start' % \
+              (port, password, method)
+        sudo("sed -i '$a %s' /etc/rc.d/rc.local" % cmd)
+        sudo("chmod +x /etc/rc.d/rc.local")
+        sudo("firewall-cmd --zone=public --add-port=%s/tcp --permanent" % port)
+
+        with settings(warn_only=True):
+            reboot()
+        sudo('ps -ef | grep ssserver')
+
+    except BaseException as e:
+        logging.error(e)
+        return False
