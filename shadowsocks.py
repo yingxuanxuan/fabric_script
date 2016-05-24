1454a795f5389f3a0b63f4250344eb679eb0f944
yingxuanxuan <yingxuanxuan@hotmail.com>
2016-05-24 22:27:09 +0800

    Complete ssh config.

--- shadowsocks.py
+++ shadowsocks.py
@@ -4,12 +4,13 @@
 import logging; logging.basicConfig(level=logging.INFO)
 from fabric.api import reboot, sudo, settings
 
-def setup(port, password, method):
+def ssserver(port, password, method):
     try:
         sudo('hash yum')
         sudo('hash python')
         sudo('yum -y update')
         sudo('yum -y install python-setuptools')
+        sudo('yum -y install m2crypto')
         sudo('easy_install pip')
         sudo('pip install shadowsocks')
         sudo('hash ssserver')
@@ -23,6 +24,33 @@ def setup(port, password, method):
         with settings(warn_only=True):
             reboot()
         sudo('ps -ef | grep ssserver')
+        return True
+
+    except BaseException as e:
+        logging.error(e)
+        return False
+
+def sslocal(server_addr, server_port, password, method, local_port):
+    try:
+        sudo('hash yum')
+        sudo('hash python')
+        sudo('yum -y update')
+        sudo('yum -y install python-setuptools')
+        sudo('yum -y install m2crypto')
+        sudo('easy_install pip')
+        sudo('pip install shadowsocks')
+        sudo('hash sslocal')
+
+        cmd = '/usr/bin/python /usr/bin/sslocal -s %s -p %s -k %s -m %s -l %s --user nobody -d start' % \
+              (server_addr, server_port, password, method, local_port)
+        sudo("sed -i '$a %s' /etc/rc.d/rc.local" % cmd)
+        sudo("chmod +x /etc/rc.d/rc.local")
+        sudo("firewall-cmd --zone=public --add-port=%s/tcp --permanent" % local_port)
+
+        with settings(warn_only=True):
+            reboot()
+        sudo('ps -ef | grep sslocal')
+        return True
 
     except BaseException as e:
         logging.error(e)
---
+++ ssh.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import logging;logging.basicConfig(level=logging.INFO)
+
+from fabric.api import sudo, settings, local, put, cd
+import os
+
+
+def _ssh_config(new_ssh_port):
+    #禁止密码登录
+    sudo("sed -i '/^PasswordAuthentication/s/yes/no/' /etc/ssh/sshd_config")
+    #禁止root登录
+    sudo("sed -i '/^#PermitRootLogin/a PermitRootLogin no' /etc/ssh/sshd_config")
+    #更换端口
+    sudo("sed -i '/^#Port/a Port %s' /etc/ssh/sshd_config" % new_ssh_port)
+    #打开防火墙
+    sudo("firewall-cmd --zone=public --add-port=%s/tcp --permanent" % new_ssh_port)
+    #关闭防火墙
+    sudo("firewall-cmd --zone=public --remove-service=ssh --permanent")
+    #重启sshd
+    sudo("systemctl restart sshd.service")
+    #重启防火墙
+    sudo("firewall-cmd --reload")
+
+
+def _ssh_keygen(passphrase, key_filename):
+    local('ssh-keygen -t rsa -N "%s" -f %s' % (passphrase, key_filename))
+
+def _add_user(adduser):
+    #检查用户是否存在
+    with settings(warn_only=True):
+        result = sudo('id %s' % adduser)
+        if result.failed:
+            logging.info('user %s not exist.' % adduser)
+        else:
+            logging.info('user %s already exist.' % adduser)
+            return
+
+    #创建用户
+    sudo('useradd %s' % adduser)
+
+    #添加到wheel组
+    sudo('sudo usermod -G wheel %s ' % (adduser))
+
+
+def set_ssh_with_upload_key(adduser, key_filename, new_ssh_port):
+    try:
+        # add user
+        _add_user(adduser)
+
+        # get home path
+        output = sudo('eval echo ~%s' % adduser)
+        homepath = output.split("/n")[0]
+        sudo("mkdir -p %s/.ssh/" % homepath)
+        # add public key
+
+        with cd('%s/.ssh/' % homepath):
+            put(key_filename + '.pub', '.')
+            sudo('cat %s.pub >> authorized_keys' % os.path.basename(key_filename))
+            sudo('chown %s:%s authorized_keys' % (adduser, adduser))
+            sudo('rm %s.pub' % os.path.basename(key_filename))
+
+        # ssh config
+        _ssh_config(new_ssh_port)
+    except BaseException as e:
+        logging.error(e)
+        return False
+    return True
+
+
+def set_ssh_with_new_key(adduser, passphrase, key_filename, new_ssh_port):
+    try:
+        _ssh_keygen(passphrase, key_filename)
+        return set_ssh_with_upload_key(adduser, key_filename, new_ssh_port)
+
+    except BaseException as e:
+        logging.error(e)
+        return False
+
